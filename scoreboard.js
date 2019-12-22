d956eb8bd2ff55c0be41722f593dc39dbba3b833
diegoami <diego.amicabile@gmail.com>
2019-12-22 20:54:29 +0100

    Work with Post as well as with Get

--- scoreboard.js
+++ scoreboard.js
@@ -4,6 +4,8 @@ const express = require("express");
 const redis = require("redis");
 const axios = require("axios");
 const bodyParser = require("body-parser");
+var _ = require("underscore");
+var cors = require('cors');
 
 //setup port constants
 const port_redis = process.env.PORT || 6379;
@@ -14,16 +16,18 @@ const app = express();
 //Body Parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors())
 
 
 
 
-app.get("/scores/:id", async (req, res) => {
+app.get("/scores/:id", cors(), async (req, res) => {
     try {
         const { id } = req.params;
         //add data to Redis
-        redis_client.zrange(id, 0, -1, function (err, obj) {
-            return res.json(obj);
+        redis_client.zrevrange(id, 0, -1, "WITHSCORES", function (err, obj) {
+            ret =  _.chunk(obj, 2).slice(0,5)
+            return res.json(ret);
         });
 
 
@@ -33,12 +37,13 @@ app.get("/scores/:id", async (req, res) => {
     }
 });
 
-app.post("/scores/:id", async (req, res) => {
+app.post("/scores/:id", cors(), async (req, res) => {
     try {
         const { id } = req.params;
 
-        var name = req.body.name;
         var score = req.body.score;
+        var name = req.body.name;
+        console.log(req.body);
 
         //add data to Redis
         redis_client.zadd(id, score, name, function (err, obj) {
