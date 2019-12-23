088dd4bb187a4172c6c1cec0e93c1fbc19c66824
diegoami <diego.amicabile@gmail.com>
2019-12-23 12:26:43 +0100

    Manage time of scores

--- scoreboard.js
+++ scoreboard.js
@@ -6,6 +6,8 @@ const axios = require("axios");
 const bodyParser = require("body-parser");
 var _ = require("underscore");
 var cors = require('cors');
+const moment = require('moment');
+moment().format();
 
 //setup port constants
 const port_redis = process.env.PORT || 6379;
@@ -13,6 +15,7 @@ const port = process.env.PORT || 5000;
 const redis_client = redis.createClient(port_redis);
 const app = express();
 
+moment().format();
 //Body Parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -26,7 +29,17 @@ app.get("/scores/:id", cors(), async (req, res) => {
         const { id } = req.params;
         //add data to Redis
         redis_client.zrevrange(id, 0, -1, "WITHSCORES", function (err, obj) {
-            ret =  _.chunk(obj, 2).slice(0,5)
+            var highest =  _.chunk(obj, 2).slice(0,5)
+            var ret = _.map(highest, function(rec) {
+                var score = rec[1];
+                var keys = rec[0].split('|');
+                var name = keys[0];
+                var score_day = keys[1];
+                return {
+                    "score" : score, "name" : name, "score_day" : score_day
+                }
+
+            })
             return res.json(ret);
         });
 
@@ -43,10 +56,11 @@ app.post("/scores/:id", cors(), async (req, res) => {
 
         var score = req.body.score;
         var name = req.body.name;
+        var key = name +'|'+moment().format("DD/MM/YYYY hh:ss");
         console.log(req.body);
 
         //add data to Redis
-        redis_client.zadd(id, score, name, function (err, obj) {
+        redis_client.zadd(id, score, key, function (err, obj) {
             return res.json(obj);
         });
 
